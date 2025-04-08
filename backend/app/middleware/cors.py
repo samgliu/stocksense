9e0d4773d98455f4eddd5093c7189f34b89cc889
Sam Liu <samgliu19@gmail.com>
2025-04-08 00:48:02 -0400

    chore: move allowed origin to env

--- backend/app/middleware/cors.py
+++ backend/app/middleware/cors.py
@@ -1,11 +1,13 @@
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
-
+import os
 
 def add_cors_middleware(app: FastAPI):
+    allow_origins = os.getenv("ALLOWED_ORIGINS", "http://localhost:3000,http://192.168.7.180:3000").split(",")
+
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["http://localhost:3000", "http://192.168.7.180:3000"],
+        allow_origins=allow_origins,
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
