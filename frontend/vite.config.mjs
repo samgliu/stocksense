194b974e86d2e595492569295fb7f0f02ef93e7b
Sam Liu <samgliu19@gmail.com>
2025-04-09 20:18:03 -0400

    chore: move to hash router

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  base: '/stocksense/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
