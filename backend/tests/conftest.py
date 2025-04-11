76619b9f42ea61eab144cf41378eabc1d7254490
Sam Liu <samgliu19@gmail.com>
2025-04-10 21:27:26 -0400

    chore: adjust test env

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,15 +1,8 @@
 import os
-import pytest
 from unittest.mock import patch
-
 from dotenv import load_dotenv
 
 load_dotenv(".env.test")
 
-
-@pytest.fixture(autouse=True, scope="session")
-def mock_firebase():
-    with patch("firebase_admin.initialize_app"), patch(
-        "firebase_admin.credentials.Certificate"
-    ):
-        yield
+patch("firebase_admin.initialize_app").start()
+patch("firebase_admin.credentials.Certificate").start()
