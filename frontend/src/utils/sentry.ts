7e2e8e16d84810f5b2728dca27336dcd606a812a
Sam Liu <samgliu19@gmail.com>
2025-04-21 19:00:54 -0400

    chore: remove session

--- frontend/src/utils/sentry.ts
+++ frontend/src/utils/sentry.ts
@@ -3,6 +3,9 @@ import * as Sentry from '@sentry/react';
 Sentry.init({
   dsn: import.meta.env.VITE_SENTRY_DSN,
   tracesSampleRate: 0,
+  integrations: (defaultIntegrations) => {
+    return defaultIntegrations.filter((integration) => integration.name !== 'BrowserSession');
+  },
   replaysSessionSampleRate: 0,
   replaysOnErrorSampleRate: 0,
   sampleRate: 0.1,
